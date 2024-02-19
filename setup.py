df1b6461b62266983e10a72a6e88e905e2f2ea0e
Max Collard <maxwell.collard@ucsf.edu>
2024-02-18 17:26:50 -0800

    Moving to cfg based setup

--- setup.py
+++ setup.py
@@ -1,39 +1,42 @@
 from setuptools import setup
 
-setup(
-    name = 'aqua-astrocytes',
-    packages = [
-        'aqua',
-    ],
+if __name__ == '__main__':
+    setup()
 
-    # TODO Pull from source
-    version = '0.1',
+# setup(
+#     name = 'aqua-astrocytes',
+#     packages = [
+#         'aqua',
+#     ],
 
-    description = 'A Python interface for astrocyte quantification and analysis',
-    # TODO Add long_description
-    # long_description = '...',
-    author = 'Max Collard',
-    author_email = 'maxwell.collard@ucsf.edu',
-    url = 'https://github.com/maxcollard/aqua-astrocytes',
-    license = 'MIT',
-    classifiers = [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Programming Language :: Python :: 3.8',
-    ],
+#     # TODO Pull from source
+#     version = '0.1',
 
-    # TODO Work through actual minimum required versions
-    install_requires = [
-        'h5py==3.9.0',
-        'numpy==1.24.3',
-        'pandas==2.0.3',
-        'pyyaml==6.0.1',
-        'scikit-learn==1.3.0',
-        'scipy==1.10.1',
-        'statsmodels==0.14.0',
-        'tqdm==4.65.0',
-    ],
-)
+#     description = 'A Python interface for astrocyte quantification and analysis',
+#     # TODO Add long_description
+#     # long_description = '...',
+#     author = 'Max Collard',
+#     author_email = 'maxwell.collard@ucsf.edu',
+#     url = 'https://github.com/maxcollard/aqua-astrocytes',
+#     license = 'MIT',
+#     classifiers = [
+#         'Development Status :: 3 - Alpha',
+#         'Intended Audience :: Science/Research',
+#         'Topic :: Scientific/Engineering :: Bio-Informatics',
+#         'Programming Language :: Python :: 3.8',
+#     ],
+
+#     # TODO Work through actual minimum required versions
+#     install_requires = [
+#         'h5py==3.9.0',
+#         'numpy==1.24.3',
+#         'pandas==2.0.3',
+#         'pyyaml==6.0.1',
+#         'scikit-learn==1.3.0',
+#         'scipy==1.10.1',
+#         'statsmodels==0.14.0',
+#         'tqdm==4.65.0',
+#     ],
+# )
 
 #
